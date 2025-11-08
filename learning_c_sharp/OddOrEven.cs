23979531c320aa4f5938e5aea90e065154446e7a
christian <christiannemeno80@gmail.com>
2025-11-08 16:39:23 +0800

    problem odd or even in geeks for geeks

---
+++ learning_c_sharp/OddOrEven.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learning_c_sharp
+{
+    public class OddOrEven
+    {
+        public bool checker(int x)
+        {
+            return x % 2 == 0;
+        }
+    }
+}
--- learning_c_sharp/Program.cs
+++ learning_c_sharp/Program.cs
@@ -1,9 +1,14 @@
 ﻿
+using learning_c_sharp;
+
+//Console.WriteLine("Hello world !");
 
 
-Console.WriteLine("Hello world !");
+//Console.WriteLine("Enter your name: ");
+//string name = Console.ReadLine();
+//Console.WriteLine($"Your name is {name}");
 
 
-Console.WriteLine("Enter your name: ");
-string name = Console.ReadLine();
-Console.WriteLine($"Your name is {name}");
+OddOrEven oddOrEven = new OddOrEven();
+
+Console.WriteLine(oddOrEven.checker(2));
