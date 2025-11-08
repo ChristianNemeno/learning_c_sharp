44ddfbcffd6bdaa80246118f4d34e45fa62904e9
christian <christiannemeno80@gmail.com>
2025-11-08 16:05:19 +0800

    math operations

--- learning_c_sharp/Program.cs
+++ learning_c_sharp/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+//int sum = 7 + 5;
+//int difference = 7 - 5;
+//int product = 7 * 5;
+//int quotient = 7 / 5;
+
+//Console.WriteLine("Sum: " + sum);
+//Console.WriteLine("Difference: " + difference);
+//Console.WriteLine("Product: " + product);
+//Console.WriteLine("Quotient: " + quotient);
+
+
+//Console.WriteLine("Add code to perform division using literal decimal data\r\n");
+
+
+//decimal decimalQuotient = 7.0m / 5;
+
+//Console.WriteLine($"Decimal quotient: {decimalQuotient}");
+
+//int first = 7; 
+//int second = 5;
+
+//decimal quotient = (decimal)first/(decimal)second;
+//Console.WriteLine(quotient);
+
+Console.WriteLine($"Modulus of 200 / 5 : {200 % 5}");
+Console.WriteLine($"Modulus of 7 / 5 : {7 % 5}");
